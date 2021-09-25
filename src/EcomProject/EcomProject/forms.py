a6eba257897840afb98033b4f90198a6434377a7
Gizmosoft <Kartikey.hebbar@gmail.com>
2021-09-25 14:36:45 +0530

    basic login authentication setup complete

--- src/EcomProject/EcomProject/forms.py
+++ src/EcomProject/EcomProject/forms.py
@@ -10,5 +10,31 @@ class ContactForm(forms.Form):
                         }
                     )
                 )
-    email    = forms.EmailField()
-    content  = forms.CharField()
+    email    = forms.EmailField(
+                widget=forms.EmailInput(
+                    attrs={
+                        "class":"form-control",  
+                        "placeholder":"E-mail"
+                        }
+                    )
+                )
+    content  = forms.CharField(
+                widget=forms.Textarea(
+                    attrs={
+                        "class":"form-control",
+                        "placeholder":"Your Message",
+                        }
+                    )
+                )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "@" in email:
+            raise forms.ValidationError("Invalid email entered!")
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
--- src/EcomProject/EcomProject/views.py
+++ src/EcomProject/EcomProject/views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
-from .forms import ContactForm
+from .forms import ContactForm, LoginForm
 
 def home_page(request):
     # create a python dictionary "context" and pass an arbitrary field value with a name ex: title
@@ -17,15 +18,51 @@ def about_page(request):
     return render(request, "home.html", context)
 
 def contact_page(request):
-    contact_form = ContactForm()    #create instance of form class
+    contact_form = ContactForm(request.POST or None)    #create instance of form class; passing data through ContactForm
     context = {
         'title': 'We are in the Contact Page.',
         'desc': 'Fill the below form to reach out to us.',
         'form': contact_form        # form is  a context variable
     }
 
-    if request.method == 'POST':
-        print(request.POST.get('fullname'))
-        print(request.POST.get('email'))
-        print(request.POST.get('content'))
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+
+    # if request.method == 'POST':
+    #     print(request.POST.get('fullname'))
+    #     print(request.POST.get('email'))
+    #     print(request.POST.get('content'))
     return render(request, "contact.html", context)
+
+def login_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        
+    form = LoginForm(request.POST or None)
+    context = {
+        "form":form
+    }
+    print(request.user.is_authenticated)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+
+        user = authenticate(request, username=username, password=password)
+        print(request.user.is_authenticated)    # Check auth status again
+        if user is not None:
+            print(request.user.is_authenticated)    # Check auth status again
+            print(user)
+            login(request, user)
+            return redirect("/")
+        else:
+            context['form'] = LoginForm()
+            print("Error logging in.")
+    
+    return render(request, "auth/login.html", context)
+
+def register_page(request):
+    form = LoginForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "auth/register.html", {})
