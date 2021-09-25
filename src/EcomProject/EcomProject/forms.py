c89af1fe01ff927298446ef5b13eea2ec0655936
Gizmosoft <Kartikey.hebbar@gmail.com>
2021-09-25 12:47:27 +0530

    django forms fixed bug

--- src/EcomProject/EcomProject/forms.py
+++ src/EcomProject/EcomProject/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    fullname = forms.CharField
-    email    = forms.EmailField
-    content  = forms.CharField
+    fullname = forms.CharField(
+                widget=forms.TextInput(
+                    attrs={
+                        "class":"form-control", 
+                        "id":"form_full_name", 
+                        "placeholder":"Full Name"
+                        }
+                    )
+                )
+    email    = forms.EmailField()
+    content  = forms.CharField()
