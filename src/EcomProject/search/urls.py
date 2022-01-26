83bc8b3a24dc377fef85fc526649b11d7e7c4e38
Gizmosoft <Kartikey.hebbar@gmail.com>
2022-01-26 18:34:54 +0530

    simple search implemented

--- src/EcomProject/search/urls.py
+++ src/EcomProject/search/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path
 from django.views.generic import detail
 
 from products.views import (
-                    ProductListView, 
                     ProductDetailSlugView,
                     )
 
@@ -10,8 +9,7 @@ from .views import SearchProductView
 
 app_name = 'products'
 urlpatterns = [
-    #path('', ProductListView.as_view(), name='list'),     # making class based view as callable
     re_path(r'^(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view(), name='detail'),
 
-    path('', SearchProductView.as_view(), name='search'),
+    path('', SearchProductView.as_view(), name='query'),
 ]
