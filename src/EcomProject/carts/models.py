6cf2d71f52bff5fcfffed79952910d0c23af53fe
Gizmosoft <Kartikey.hebbar@gmail.com>
2022-02-04 11:15:06 +0530

    CartManager updated

--- src/EcomProject/carts/models.py
+++ src/EcomProject/carts/models.py
@@ -1,12 +1,29 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save
 from products.models import Product
 
 User = settings.AUTH_USER_MODEL     #importing user: this will go by the default user model in the settings or any customized user model that we might have created
 
 # Create your models here.
 class CartManager(models.Model):
+    # the below method is a customized version of in built function "def get_or_create"
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = Cart.objects.filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False     # shows that new cart object is not created
+            cart_obj = qs.first()
+            print("Cart ID exists...")
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.obj.new_cart(user=request.user)
+            new_obj = True      # Shows that new cart object is created
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new_cart(self, user=None):
         user_obj = None
         if user is not None:
--- src/EcomProject/carts/views.py
+++ src/EcomProject/carts/views.py
@@ -3,36 +3,50 @@ from django.shortcuts import render
 from .models import Cart
 # Create your views here.
 
-def cart_create():
-    cart_obj = Cart.objects.create(user=None)
-    print('New cart created!')
-    return cart_obj
+## The logic of the below method has been moved to models.py
+# def cart_create():
+#     cart_obj = Cart.objects.create(user=None)
+#     print('New cart created!')
+#     return cart_obj
 
 ## We want the cart views to be only visible to the logged in customers
 ## We would use Django Sessions to do that
 ## Session object is on the request object by default
 def cart_home(request):
+    cart_obj, new_obj = Cart.obj.new_or_get(request)
+
+## Below is a very simple and inefficient way of calculating cart total price
+    products = cart_obj.product.all()
+    total = 0
+    # Calculate total cart price
+    for p in products:
+        total += p.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
+
+# All the lines/snippets marked with --> have been moved to the CartManager model simplifying the views.
     #del request.session['cart_id']      # delete the previous session id
     #request.session['cart_id'] = "12"   # setting cart id as a test
-    cart_id = request.session.get("cart_id", None)  # get current cart ID or else show none
+    #--> cart_id = request.session.get("cart_id", None)  # get current cart ID or else show none
     # if cart_id is None and isinstance(cart_id, int):    # isinstance checks if the cart_id is of type integer
     #     cart_obj = cart_create()
     #     request.session['cart_id'] = cart_obj.id 
     # else:
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        cart_obj = qs.first()
-        print("Cart ID exists...")
+    #--> qs = Cart.objects.filter(id=cart_id)
+    # if qs.count() == 1:
+    #     cart_obj = qs.first()
+    #-->     print("Cart ID exists...")
         # print(cart_id)
         # cart_obj = Cart.objects.get(id=cart_id)
         # print(cart_obj)
         ## below condition assigns an already created cart when user was not logged in to the user once they login
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.obj.new_cart(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    #-->     if request.user.is_authenticated and cart_obj.user is None:
+    #         cart_obj.user = request.user
+    #-->         cart_obj.save()
+    #--> else:
+    #     cart_obj = Cart.obj.new_cart(user=request.user)
+    #-->     request.session['cart_id'] = cart_obj.id
     #print(request.session)
     #print(dir(request.session)) # helps us inspect the various method on the session module
     # key = request.session.session_key
